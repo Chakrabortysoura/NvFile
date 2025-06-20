49017ba923fcb92c61ee8c0d8a0fd8ebd48d47dc
Souranil <chakrabortysoura21@gmail.com>
2025-06-20 19:20:35 +0530

    Keyboard shortucts config file integrated. Config files are now moved to user config folder.

--- ui/config.go
+++ ui/config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/charmbracelet/lipgloss"
 	"os"
+	"sync"
 )
 
 var (
@@ -14,25 +15,27 @@ var (
 		"dirColor":        {"#545755"},
 		"errorColor":      {"#ff0033"},
 	}
-	//keybindconfig = map[string][]string{
-	//	"toggleHiddenFile": {"ctrl+h"},
-	//	"down":             {"j", "down"},
-	//	"up":               {"k", "up"},
-	//	"newDile":          {"ctrl+n"},
-	//	"newSubDir":        {"ctrl+d"},
-	//	"goBack":           {"ctrl+b", "backspace"},
-	//	"deleteFileorDir":  {"delete"},
-	//	"exit":             {"ctrl+z", "ctrl+q"},
-	//}
+	keybindconfig = map[string][]string{
+		"togglehiddenfile": {"ctrl+h"},
+		"down":             {"j", "down"},
+		"up":               {"k", "up"},
+		"newfile":          {"ctrl+n"},
+		"newsubdir":        {"ctrl+d"},
+		"goback":           {"ctrl+b", "backspace"},
+		"deletefileordir":  {"delete"},
+		"exit":             {"ctrl+z", "ctrl+q"},
+		"action":           {"enter"},
+	}
 )
 
-func InitColorConfig() {
+func InitColorConfig(wg *sync.WaitGroup) {
+	defer wg.Done()
 	//Opens the colorconfig file and reads the color config data from it
-	configfile, err := os.Open("colorconfig.json")
+	configfile, err := os.Open(os.Getenv("HOME") + "/.config/nvfile_colorconfig.json")
 	if os.IsNotExist(err) {
 		//If no colorconfig.json file exists then create a new config file and put the default
 		//json encoded config data in it
-		newconfigfile, err := os.Create("colorconfig.json")
+		newconfigfile, err := os.Create(os.Getenv("HOME") + "/.config/nvfile_colorconfig.json")
 		if err != nil {
 			fmt.Println("Unable to create a new colorconfig file")
 		}
@@ -51,10 +54,37 @@ func InitColorConfig() {
 		fmt.Println("Unable to read from the colorconfig file")
 	}
 
-	//Initialized the ui component renders with the new color cofig data
+	//Initialize the ui component renders with the new color cofig data
 	//after reading from config file
 	currDir = currDir.Background(lipgloss.Color(configData["bottombarFirst"][0]))
 	bottomSecond = bottomSecond.Background(lipgloss.Color(configData["bottombarSecond"][0]))
 	errorRender = errorRender.Background(lipgloss.Color(configData["errorColor"][0]))
 	dirRender = dirRender.Background(lipgloss.Color(configData["dirColor"][0]))
+}
+
+func InitKeyConfig(wg *sync.WaitGroup) {
+	defer wg.Done()
+	//Opens the keybindconfig file and reads the keyboard shortcut config data from it
+	configfile, err := os.Open(os.Getenv("HOME") + "/.config/nvfile_keybindconfig.json")
+	if os.IsNotExist(err) {
+		//If no keybindconfig.json file exists then create a new config file and put the default
+		//json encoded config data in it
+		newconfigfile, err := os.Create(os.Getenv("HOME") + "/.config/nvfile_keybindconfig.json")
+		if err != nil {
+			fmt.Println("Unable to create a new keybindconfig file")
+		}
+		defer newconfigfile.Close()
+		encoder := json.NewEncoder(newconfigfile)
+		err = encoder.Encode(keybindconfig)
+		if err != nil {
+			fmt.Println("Unable to create a new keybindconfig file")
+		}
+		return
+	}
+	defer configfile.Close()
+	decoder := json.NewDecoder(configfile) //Reads the json config data from the keybindconfig file
+	err = decoder.Decode(&keybindconfig)   // Decode the json config file from the keybindconfig file and stores the specified keybindings
+	if err != nil {
+		fmt.Println("Unable to read from the keybindconfig file")
+	}
 }
